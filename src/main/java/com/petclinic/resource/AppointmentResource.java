77a5e3e38142b3f2b9db614c5c85af21e8d3b65a
Npx <cheyprivado@gmail.com>
2026-05-22 00:41:16 -0300

    refactor

--- src/main/java/com/petclinic/resource/AppointmentResource.java
+++ src/main/java/com/petclinic/resource/AppointmentResource.java
@@ -4,14 +4,11 @@ import com.petclinic.domain.Appointment;
 import com.petclinic.resource.dto.AppointmentRequest;
 import com.petclinic.resource.dto.AppointmentResponse;
 import com.petclinic.respository.AppointmentRepository;
-import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
--- src/main/java/com/petclinic/respository/AppointmentRepository.java
+++ src/main/java/com/petclinic/respository/AppointmentRepository.java
@@ -2,15 +2,12 @@ package com.petclinic.respository;
 
 import com.petclinic.domain.Appointment;
 import com.petclinic.resource.dto.AppointmentResponse;
-import org.jspecify.annotations.NonNull;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface AppointmentRepository extends MongoRepository<Appointment, UUID> {
   List<AppointmentResponse> findByDateTimeBefore(LocalDateTime dateTimeBefore);
-  Optional<Appointment> findById(@NonNull UUID id);
 }
